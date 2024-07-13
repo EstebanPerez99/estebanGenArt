6561c1d552503cc7ff73738042f2bdb3f143b85c
EstebanPerez99 <epm1520@gmail.com>
2024-07-12 22:29:33 -0600

    mejorar posicion

--- sketch_tenis_ball/sketch.js
+++ sketch_tenis_ball/sketch.js
@@ -1,89 +1,99 @@
 function setup() {
-  const canvasSize = random(500, 1000);
-  createCanvas(canvasSize, canvasSize);
-  rectMode(CENTER);
+	randomSeed(1);
+	const canvasSize = random(500, 1000);
+	createCanvas(canvasSize, canvasSize);
+	rectMode(CENTER);
 }
 
 function draw() {
-  const rectPosX = -100;
-  const rectPosY = height / 2;
-  const rectWidth = mouseX;
-  const rectHeigth = height * 0.5;
-  const circleCenterX = rectWidth / 2 + rectPosX;
+	const mouseXWithLimit = min(width, mouseX);
+	const PADDING = width * 0.2;
+	const xPointer = map(mouseXWithLimit, 0, width, 0 + PADDING, width - PADDING);
+	const DISTANCIA_ENTRE_FIGURAS = width + width * 0.9;
 
-  noStroke();
-  background(220);
-  fill(244, 226, 49);
-  circle(width / 2, height / 2, width * 0.98);
-  noFill();
-  stroke(255);
-  strokeWeight(1);
+	let rectPosX = xPointer - DISTANCIA_ENTRE_FIGURAS / 2;
+	let rectPosY = height / 2;
+	let rectWidth = width * 0.5;
+	let rectHeigth = height * 0.8;
+	let circleCenterX = rectWidth / 2 + rectPosX;
 
-  const inner_offset = width * 0.025;
-  const inner_offset_half = inner_offset / 2;
+	noStroke();
+	background(220);
+	fill(244, 226, 49);
+	circle(width / 2, height / 2, width * 0.98);
+	noFill();
+	stroke(255);
+	strokeWeight(1);
 
-  rect(rectPosX, rectPosY, rectWidth, rectHeigth);
-  circle(circleCenterX, rectPosY, rectHeigth);
-  rect(rectPosX, rectPosY, rectWidth, rectHeigth - inner_offset);
-  circle(circleCenterX, rectPosY, rectHeigth - inner_offset);
+	const inner_offset = width * 0.025;
+	const inner_offset_half = inner_offset / 2;
 
-  const curve_offset = width * 0.175;
-  const curve_offset2 = width * 0.181;
-  // circulo de arriba
-  circle(
-    circleCenterX - curve_offset,
-    rectPosY - rectHeigth + inner_offset_half,
-    rectHeigth
-  );
-  circle(
-    circleCenterX - curve_offset2,
-    rectPosY - rectHeigth + inner_offset_half,
-    rectHeigth - inner_offset
-  );
-  // circulo de abajo
-  circle(
-    circleCenterX - curve_offset,
-    rectPosY + rectHeigth - inner_offset_half,
-    rectHeigth
-  );
-  circle(
-    circleCenterX - curve_offset2,
-    rectPosY + rectHeigth - inner_offset_half,
-    rectHeigth - inner_offset
-  );
+	rect(rectPosX, rectPosY, rectWidth, rectHeigth);
+	circle(circleCenterX, rectPosY, rectHeigth);
+	rect(rectPosX, rectPosY, rectWidth, rectHeigth - inner_offset);
+	circle(circleCenterX, rectPosY, rectHeigth - inner_offset);
 
-  // -------- MIRRROR --------
+	const curve_offset = width * 0.175;
+	const curve_offset2 = width * 0.181;
+	// circulo de arriba
+	circle(
+		circleCenterX - curve_offset,
+		rectPosY - rectHeigth + inner_offset_half,
+		rectHeigth
+	);
+	circle(
+		circleCenterX - curve_offset2,
+		rectPosY - rectHeigth + inner_offset_half,
+		rectHeigth - inner_offset
+	);
+	// circulo de abajo
+	circle(
+		circleCenterX - curve_offset,
+		rectPosY + rectHeigth - inner_offset_half,
+		rectHeigth
+	);
+	circle(
+		circleCenterX - curve_offset2,
+		rectPosY + rectHeigth - inner_offset_half,
+		rectHeigth - inner_offset
+	);
 
-  rect(rectPosX + width, rectPosY, width - mouseX, rectHeigth);
-  circle(circleCenterX + width / 2, rectPosY, rectHeigth);
-  rect(rectPosX + width, rectPosY, width - mouseX, rectHeigth - inner_offset);
-  circle(circleCenterX + width / 2, rectPosY, rectHeigth - inner_offset);
-  // circulo de arriba
-  circle(
-    circleCenterX + width / 2 + curve_offset,
-    rectPosY - rectHeigth + inner_offset_half,
-    rectHeigth
-  );
-  circle(
-    circleCenterX + width / 2 + curve_offset2,
-    rectPosY - rectHeigth + inner_offset_half,
-    rectHeigth - inner_offset
-  );
-  // circulo de abajo
-  circle(
-    circleCenterX + width / 2 + curve_offset,
-    rectPosY + rectHeigth - inner_offset_half,
-    rectHeigth
-  );
-  circle(
-    circleCenterX + width / 2 + curve_offset2,
-    rectPosY + rectHeigth - inner_offset_half,
-    rectHeigth - inner_offset
-  );
+	// -------- MIRRROR --------
+
+	rectPosX = xPointer + DISTANCIA_ENTRE_FIGURAS / 2;
+	// rectWidth = width / 2;
+	circleCenterX = rectPosX - rectWidth / 2;
+
+	rect(rectPosX, rectPosY, rectWidth, rectHeigth);
+	circle(circleCenterX, rectPosY, rectHeigth);
+	rect(rectPosX, rectPosY, rectWidth, rectHeigth - inner_offset);
+	circle(circleCenterX, rectPosY, rectHeigth - inner_offset);
+	// circulo de arriba
+	circle(
+		circleCenterX + curve_offset,
+		rectPosY - rectHeigth + inner_offset_half,
+		rectHeigth
+	);
+	circle(
+		circleCenterX + curve_offset2,
+		rectPosY - rectHeigth + inner_offset_half,
+		rectHeigth - inner_offset
+	);
+	// circulo de abajo
+	circle(
+		circleCenterX + curve_offset,
+		rectPosY + rectHeigth - inner_offset_half,
+		rectHeigth
+	);
+	circle(
+		circleCenterX + curve_offset2,
+		rectPosY + rectHeigth - inner_offset_half,
+		rectHeigth - inner_offset
+	);
 }
 
 function keyTyped() {
-  if (key === "S") {
-    saveCanvas("ball_" + random(1, 100000), "jpeg");
-  }
+	if (key === "S") {
+		saveCanvas("ball_" + random(1, 100000), "jpeg");
+	}
 }
