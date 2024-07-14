e178ddf947df392306816fde46672f525f6f8a08
EstebanPerez99 <epm1520@gmail.com>
2024-07-13 23:25:18 -0600

    seed points & flood fill

--- sketch_tenis_ball/sketch.js
+++ sketch_tenis_ball/sketch.js
@@ -1,40 +1,70 @@
+let INCLINATION = 0;
+let CONFIGS = { originX: 0, originY: 0 };
 function setup() {
 	randomSeed(1);
-	const canvasSize = random(500, 1000);
+	const canvasSize = round(random(500, 1000));
 	createCanvas(canvasSize, canvasSize);
 	rectMode(CENTER);
+	angleMode(DEGREES);
+	noLoop();
 }
 
 function draw() {
-	const mouseXWithLimit = min(width, mouseX);
-	const PADDING = width * 0.2;
-	const xPointer = map(mouseXWithLimit, 0, width, 0 + PADDING, width - PADDING);
-	const DISTANCIA_ENTRE_FIGURAS = width + width * 0.9;
+	background(220);
 
-	let rectPosX = xPointer - DISTANCIA_ENTRE_FIGURAS / 2;
-	let rectPosY = height / 2;
-	let rectWidth = width * 0.5;
-	let rectHeigth = height * 0.8;
-	let circleCenterX = rectWidth / 2 + rectPosX;
+	fill(255, 0, 0);
+	textSize(20);
+	textAlign(LEFT, TOP);
+	text(INCLINATION, 10, 10);
+
+	push();
+
+	const middle = width / 2;
 
 	noStroke();
-	background(220);
 	fill(244, 226, 49);
-	circle(width / 2, height / 2, width * 0.98);
+	const ballDiameter = width * 0.98;
+	circle(middle, middle, ballDiameter);
 	noFill();
-	stroke(255);
-	strokeWeight(1);
+	stroke(255, 0, 0);
+	circle(middle, middle, 5);
+
+	const mouseXWithLimit = min(width, mouseX);
+	const mouseYHeightLimit = min(height, mouseY);
+	const PADDING = width * 0; // 0.2;
+	const xPointer = map(mouseXWithLimit, 0, width, PADDING, width - PADDING);
+	const yPointer = map(mouseYHeightLimit, 0, height, PADDING, height - PADDING);
+	translate(xPointer, yPointer);
+	CONFIGS = { originX: xPointer, originY: yPointer };
+	const DISTANCIA_ENTRE_FIGURAS = width + width * 0.1; // 0.9
+
+	let rectPosX = -DISTANCIA_ENTRE_FIGURAS / 2;
+	let rectPosY = 0;
+	let rectWidth = width * 0.5;
+	let rectHeigth = height * 0.3; // 0.8
+	let circleCenterX = rectWidth / 2 + rectPosX;
+
+	push();
+	noStroke();
+	fill(0, 0, 255);
+	circle(0, 0, 5);
+	pop();
 
 	const inner_offset = width * 0.025;
 	const inner_offset_half = inner_offset / 2;
 
+	push(); // (start) ESQUELETO  **********
+	rotate(INCLINATION);
+	stroke(255, 0, 0);
+	strokeWeight(1);
 	rect(rectPosX, rectPosY, rectWidth, rectHeigth);
-	circle(circleCenterX, rectPosY, rectHeigth);
 	rect(rectPosX, rectPosY, rectWidth, rectHeigth - inner_offset);
+	circle(circleCenterX, rectPosY, rectHeigth);
 	circle(circleCenterX, rectPosY, rectHeigth - inner_offset);
 
 	const curve_offset = width * 0.175;
 	const curve_offset2 = width * 0.181;
+
 	// circulo de arriba
 	circle(
 		circleCenterX - curve_offset,
@@ -58,9 +88,47 @@ function draw() {
 		rectHeigth - inner_offset
 	);
 
+	doFloodFill(createVector(circleCenterX + 3, -rectHeigth / 2 + 3));
+	// doFloodFill(createVector(circleCenterX - 3, -rectHeigth / 2 + 3));
+	// doFloodFill(createVector(circleCenterX - curve_offset, -rectHeigth / 2 + 3));
+	// const puntosEq1 = generarPuntosEquidistantes(
+	// 	{ x: circleCenterX - 3, y: -rectHeigth / 2 + 3 },
+	// 	{ x: circleCenterX - curve_offset, y: -rectHeigth / 2 + 3 },
+	// 	10,
+	// 	false
+	// );
+	// puntosEq1.forEach((p) => {
+	// 	doFloodFill(p);
+	// });
+
+	// doFloodFill(createVector(circleCenterX - 3, rectHeigth / 2 - 3));
+	// doFloodFill(createVector(circleCenterX - curve_offset, rectHeigth / 2 - 3));
+	// const puntosEq2 = generarPuntosEquidistantes(
+	// 	{ x: circleCenterX - 3, y: rectHeigth / 2 - 3 },
+	// 	{ x: circleCenterX - curve_offset, y: rectHeigth / 2 - 3 },
+	// 	10,
+	// 	false
+	// );
+	// puntosEq2.forEach((p) => {
+	// 	doFloodFill(p);
+	// });
+
+	// doFloodFill(
+	// 	createVector(
+	// 		circleCenterX - curve_offset - rectHeigth / 2 + 2,
+	// 		rectPosY - rectHeigth + inner_offset_half + 3
+	// 	)
+	// );
+	// doFloodFill(
+	// 	createVector(
+	// 		circleCenterX - curve_offset - rectHeigth / 2 + 2,
+	// 		rectPosY + rectHeigth - inner_offset_half - 3
+	// 	)
+	// );
+
 	// -------- MIRRROR --------
 
-	rectPosX = xPointer + DISTANCIA_ENTRE_FIGURAS / 2;
+	rectPosX = DISTANCIA_ENTRE_FIGURAS / 2;
 	// rectWidth = width / 2;
 	circleCenterX = rectPosX - rectWidth / 2;
 
@@ -68,6 +136,7 @@ function draw() {
 	circle(circleCenterX, rectPosY, rectHeigth);
 	rect(rectPosX, rectPosY, rectWidth, rectHeigth - inner_offset);
 	circle(circleCenterX, rectPosY, rectHeigth - inner_offset);
+
 	// circulo de arriba
 	circle(
 		circleCenterX + curve_offset,
@@ -90,10 +159,129 @@ function draw() {
 		rectPosY + rectHeigth - inner_offset_half,
 		rectHeigth - inner_offset
 	);
+
+	// doFloodFill(createVector(circleCenterX - 5, -rectHeigth / 2 + 3));
+	// doFloodFill(createVector(circleCenterX + 3, -rectHeigth / 2 + 3));
+	// doFloodFill(createVector(circleCenterX + curve_offset, -rectHeigth / 2 + 3));
+	// const puntosEq3 = generarPuntosEquidistantes(
+	// 	{ x: circleCenterX + 3, y: -rectHeigth / 2 + 3 },
+	// 	{ x: circleCenterX + curve_offset, y: -rectHeigth / 2 + 3 },
+	// 	10,
+	// 	false
+	// );
+	// puntosEq3.forEach((p) => {
+	// 	doFloodFill(p);
+	// });
+
+	// doFloodFill(createVector(circleCenterX + 3, rectHeigth / 2 - 3));
+	// doFloodFill(createVector(circleCenterX + curve_offset, rectHeigth / 2 - 3));
+	// const puntosEq4 = generarPuntosEquidistantes(
+	// 	{ x: circleCenterX + 3, y: rectHeigth / 2 - 3 },
+	// 	{ x: circleCenterX + curve_offset, y: rectHeigth / 2 - 3 },
+	// 	10,
+	// 	false
+	// );
+	// puntosEq4.forEach((p) => {
+	// 	doFloodFill(p);
+	// });
+	// doFloodFill(
+	// 	createVector(
+	// 		circleCenterX + curve_offset + rectHeigth / 2 - 2,
+	// 		rectPosY - rectHeigth + inner_offset_half + 3
+	// 	)
+	// );
+	doFloodFill(
+		createVector(
+			circleCenterX + curve_offset + rectHeigth / 2 - 2,
+			rectPosY + rectHeigth - inner_offset_half - 3
+		)
+	);
+	pop(); // (end) ESQUELETO **********
+	pop();
 }
 
 function keyTyped() {
 	if (key === "S") {
 		saveCanvas("ball_" + random(1, 100000), "jpeg");
 	}
+}
+
+function keyPressed() {
+	if (keyCode === UP_ARROW) {
+		INCLINATION += 10;
+	} else if (keyCode === DOWN_ARROW) {
+		INCLINATION -= 10;
+	}
+	if (INCLINATION < 0) {
+		INCLINATION = 360;
+	}
+	if (INCLINATION > 360) {
+		INCLINATION = 0;
+	}
+}
+
+// function doFloodFill(seed) {
+// 	seed = createVector(round(seed.x), round(seed.y));
+// 	print(seed.x, seed.y);
+// 	push();
+// 	noStroke();
+// 	fill(0);
+// 	circle(seed.x, seed.y, 2);
+// 	pop();
+// }
+
+function doFloodFill(seed, fillColor = [255, 255, 255]) {
+	push();
+	translate(-CONFIGS.originX, -CONFIGS.originY);
+	seed = createVector(
+		round(seed.x + CONFIGS.originX),
+		round(seed.y + CONFIGS.originY)
+	);
+	if (seed.x > width || seed.x < 0 || seed.y > height || seed.y < 0) {
+		pop();
+		return;
+	}
+	loadPixels();
+
+	let index = 4 * (width * seed.y + seed.x);
+	// print("seed.x: ", seed.x);
+	// print("seed.y: ", seed.y);
+	let seedColor = [pixels[index], pixels[index + 1], pixels[index + 2]];
+
+	// solo se pinta si el seed color es diferente al fill color y si el seed color es amarillo.
+	if (u_arrayEquals(seedColor, fillColor)) {
+		pop();
+		return;
+	}
+
+	let queue = [];
+	queue.push(seed);
+
+	while (queue.length) {
+		let current = queue.shift();
+		index = 4 * (width * current.y + current.x);
+		let color = [pixels[index], pixels[index + 1], pixels[index + 2]];
+
+		// solo se pinta si el color es igual al seed color (aka amarillo)
+		if (
+			!u_arrayEquals(color, seedColor) &&
+			!u_arrayEquals(color, [244, 226, 49]) // esto en teoria deberia funcionar, pero hay colores intermedios que hacen que no jale D:
+		) {
+			// logear para ver en que color se para
+			// console.log("color: ", color);
+			continue;
+		}
+
+		for (let i = 0; i < 4; i++) {
+			if (i === 3) {
+				pixels[index + i] = 255;
+			}
+			pixels[index + i] = fillColor[0 + i];
+		}
+
+		queue = u_expandToNeighbors(queue, current);
+	}
+
+	pop();
+	updatePixels();
 }
--- tenis_project/sketch.js
+++ tenis_project/sketch.js
@@ -6,106 +6,106 @@ const FONDOS = [0, "rgb(25, 119, 191)"];
 const DRAW_GRID = false;
 const DRAW_LINEAS_PERSPECTIVA = true;
 function setup() {
-  noiseSeed(1);
-  // randomSeed(1);
-  angleMode(DEGREES);
-  rectMode(CENTER);
-  noLoop();
+	noiseSeed(1);
+	// randomSeed(1);
+	angleMode(DEGREES);
+	rectMode(CENTER);
+	noLoop();
 
-  createCanvas(700, 700);
-  background(color(FONDOS[1]));
+	createCanvas(700, 700);
+	background(color(FONDOS[1]));
 }
 
 function draw() {
-  // nos vamos a desplazar para poder tener un contorno
-  const numberOfCells = 20;
-  const cellW = width / numberOfCells;
-  const cellH = height / numberOfCells;
-  push();
-  translate(-cellW, -cellH);
-  // dibujar grid
-  const gridPoints = crearGrid(numberOfCells, numberOfCells, DRAW_GRID, 2);
+	// nos vamos a desplazar para poder tener un contorno
+	const numberOfCells = 20;
+	const cellW = width / numberOfCells;
+	const cellH = height / numberOfCells;
+	push();
+	translate(-cellW, -cellH);
+	// dibujar grid
+	const gridPoints = crearGrid(numberOfCells, numberOfCells, DRAW_GRID, 2);
 
-  console.log(gridPoints);
-  const baseLines = getBaseLines(gridPoints);
-  console.log(baseLines);
-  // dibujar linea
-  // let p1 = gridPoints[gridPoints.length - 1][0];
-  // let p2 = gridPoints[0][gridPoints.length - 1];
-  // const linea1 = new LineaGrid(p1, p2);
-  if (DRAW_LINEAS_PERSPECTIVA) {
-    // linea1.dibujarLineaConPerspectiva();
-    baseLines.forEach((l) => l.dibujarLineaConPerspectiva());
-  }
-  pop();
+	console.log(gridPoints);
+	const baseLines = getBaseLines(gridPoints);
+	console.log(baseLines);
+	// dibujar linea
+	// let p1 = gridPoints[gridPoints.length - 1][0];
+	// let p2 = gridPoints[0][gridPoints.length - 1];
+	// const linea1 = new LineaGrid(p1, p2);
+	if (DRAW_LINEAS_PERSPECTIVA) {
+		// linea1.dibujarLineaConPerspectiva();
+		baseLines.forEach((l) => l.dibujarLineaConPerspectiva());
+	}
+	pop();
 }
 
 /**
  * 1. Crear Grid
  */
 const crearGrid = (grid_w, grid_h, draw = false, padding = 0) => {
-  const gridPoints = u_grid(grid_w, grid_h, draw, padding).map((rows) =>
-    rows.map((cell) => {
-      let nx = NOISE_WEIGHTS_SCALE * cell.x;
-      let ny = NOISE_WEIGHTS_SCALE * cell.y;
-      let weight = sq(round(noise(nx, ny) * 6));
+	const gridPoints = u_grid(grid_w, grid_h, draw, padding).map((rows) =>
+		rows.map((cell) => {
+			let nx = NOISE_WEIGHTS_SCALE * cell.x;
+			let ny = NOISE_WEIGHTS_SCALE * cell.y;
+			let weight = sq(round(noise(nx, ny) * 6));
 
-      return new GridPoint(cell.x, cell.y, weight, draw);
-    })
-  );
+			return new GridPoint(cell.x, cell.y, weight, draw);
+		})
+	);
 
-  return gridPoints;
+	return gridPoints;
 };
 
 const getBaseLines = (gridPoints) => {
-  const baseLines = [];
-  const cols = gridPoints[0].length;
-  const rows = gridPoints.length;
-  const centerCol = round(cols / 2) - 1;
-  const centerRow = round(rows / 2) - 1;
-  for (let i = 0; i < round(random(1, 3)); i++) {
-    if (random() >= 0.5) {
-      // horizontal
-      // la variacion en y es menor
-      // se toman minimos y maximos en cols
-      let startingPoint = round(random(rows - 1));
-      let p1 = gridPoints[startingPoint][0];
-      let endingPoint = _calcularInclinacion(
-        startingPoint,
-        round(random(0, centerRow)),
-        rows - 1
-      );
-      let p2 = gridPoints[endingPoint][cols - 1];
-      baseLines.push(new LineaGrid(p1, p2));
-    } else {
-      // vertical
-      // la variacion en x es menor
-      // se toman minimos y maximos en rows
-      let startingPoint = round(random(cols - 1));
-      let p1 = gridPoints[0][startingPoint];
-      let endingPoint = _calcularInclinacion(
-        startingPoint,
-        round(random(0, centerCol)),
-        cols - 1
-      );
-      let p2 = gridPoints[rows - 1][endingPoint];
-      baseLines.push(new LineaGrid(p1, p2));
-    }
-  }
-  return baseLines;
+	const baseLines = [];
+	const cols = gridPoints[0].length;
+	const rows = gridPoints.length;
+	const centerCol = round(cols / 2) - 1;
+	const centerRow = round(rows / 2) - 1;
+	for (let i = 0; i < round(random(1, 3)); i++) {
+		if (random() >= 0.5) {
+			// horizontal
+			// la variacion en y es menor
+			// se toman minimos y maximos en cols
+			let startingPoint = round(random(rows - 1));
+			let p1 = gridPoints[startingPoint][0];
+			let endingPoint = _calcularInclinacion(
+				startingPoint,
+				round(random(0, centerRow)),
+				rows - 1
+			);
+			let p2 = gridPoints[endingPoint][cols - 1];
+			baseLines.push(new LineaGrid(p1, p2));
+		} else {
+			// vertical
+			// la variacion en x es menor
+			// se toman minimos y maximos en rows
+			let startingPoint = round(random(cols - 1));
+			let p1 = gridPoints[0][startingPoint];
+			let endingPoint = _calcularInclinacion(
+				startingPoint,
+				round(random(0, centerCol)),
+				cols - 1
+			);
+			let p2 = gridPoints[rows - 1][endingPoint];
+			baseLines.push(new LineaGrid(p1, p2));
+		}
+	}
+	return baseLines;
 };
 
 function _calcularInclinacion(start, distancia, limite) {
-  let res = start + distancia <= limite ? start + distancia : start - distancia;
-  if (res < 0) {
-    res = random() >= 0.5 ? limite : 0;
-  }
+	let res = start + distancia <= limite ? start + distancia : start - distancia;
+	if (res < 0) {
+		res = random() >= 0.5 ? limite : 0;
+	}
 
-  return res;
+	return res;
 }
 
 function keyTyped() {
-  if (key === "S") {
-    save("img");
-  }
+	if (key === "S") {
+		save("img");
+	}
 }
